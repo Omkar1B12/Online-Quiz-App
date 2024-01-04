3fe0844547b85c783ab0899d54e14f410acd43a5
Omkar Phadte <omkarphadte64@gmail.com>
2024-01-04 14:03:05 +0530

    day commit 2

--- app/src/main/java/com/example/quizonline/QuizActivity.kt
+++ app/src/main/java/com/example/quizonline/QuizActivity.kt
@@ -2,10 +2,13 @@ package com.example.quizonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizonline.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
+    lateinit var  binding:ActivityQuizBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityQuizBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_quiz)
     }
 }
--- app/src/main/java/com/example/quizonline/QuizModel.kt
+++ app/src/main/java/com/example/quizonline/QuizModel.kt
@@ -4,6 +4,15 @@ data class QuizModel(
     val id:String,
     val title:String,
     val subtitle:String,
-    val time:String){
-    constructor():this("","","","")
+    val time:String,
+    val questionList:List<QuestionModel>
+){
+    constructor():this("","","","", emptyList())
+}
+data class QuestionModel(
+    val question:String,
+    val options:List<String>,
+    val correct:String,
+){
+    constructor():this("", emptyList(),"")
 }
